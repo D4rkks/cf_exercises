503416d3244e3d7571f6d92f6861a9914481c917
Gustavo L. <44736381+D4rkks@users.noreply.github.com>
2024-10-27 22:50:25 -0300

    Create my_template.cpp

---
+++ codeforces/my_template.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl '\n'
+#define ll long long
+
+void solve(){
+
+}
+
+signed main(){
+    cin.tie(0); ios::sync_with_stdio(0);
+    int t = 0; cin >> t;
+    while(t--){
+        solve();
+    }
+}
